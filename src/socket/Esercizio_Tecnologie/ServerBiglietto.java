d2755fbfd61052fabead8adda3ed74b730c145b2
Luca Ferroni <luca@befair.it>
2016-12-07 23:48:07 +0100

    [esercizio Guion] Client e Server in Java per numero progressivo
    Il client si connette e
    il server restituisce il numero del biglietto progressivo

---
+++ src/socket/Esercizio_Tecnologie/ServerBiglietto.java
@@ -0,0 +1,41 @@
+import java.net.*;
+import java.io.*;
+
+public class ServerBiglietto
+{
+	public static void main(String args[])
+	{
+		ServerSocket sSocket;
+		Socket connessione;
+		int porta = 3333;
+		int n = 1;
+
+		//Stream per gestire il flusso in output
+		OutputStream out;
+		PrintWriter sOUT;
+
+		try
+		{
+			sSocket = new ServerSocket(porta);
+			while(true)
+			{
+				System.out.println("In attesa di connessioni...");
+				connessione = sSocket.accept();
+				System.out.println("Connessione stabilita.");
+				out = connessione.getOutputStream();
+				sOUT = new PrintWriter(out);
+				//Invia l'informazione al Client
+				String numero = Integer.toString(n);
+				sOUT.println(numero);
+				sOUT.close();
+				connessione.close();
+				System.out.println("Connessione chiusa.");
+				n += 1;
+			}
+		}
+		catch (IOException e)
+		{
+			System.err.println(e);
+		}
+	}
+}
