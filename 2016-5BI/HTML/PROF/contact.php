8fd1ff7744417e0d7093fbb03c79b8aa6660417e
Luca Ferroni <luca@befair.it>
2017-04-21 23:51:49 +0200

    [HTML] impostati i campi della form come obbligatori
    Attributi required settati al valore "required" per i campi della form.
    V. https://www.w3schools.com/tags/tag_input.asp

--- 2016-5BI/HTML/PROF/contact.php
+++ 2016-5BI/HTML/PROF/contact.php
@@ -18,16 +18,16 @@
       <form method="POST">
         <fieldset class="form-group">
           <label for="the-name">Name</label>
-          <input type="text" class="form-control" id="the-name" name="ilnome" placeholder="Enter name">
+          <input type="text" class="form-control" id="the-name" name="ilnome" placeholder="Enter name" required="required">
         </fieldset>
         <fieldset class="form-group">
           <label for="the-email">Email address</label>
-          <input type="email" class="form-control" id="the-email" name="lamail" placeholder="Enter email">
+          <input type="email" class="form-control" id="the-email" name="lamail" placeholder="Enter email" required="required">
           <small class="text-muted">We'll never share your email with anyone else.</small>
         </fieldset>
         <fieldset class="form-group">
           <label for="campotextarea1">Comment</label>
-          <textarea class="form-control" id="campotextarea1" rows="3" name="ilcommento" placeholder="Metti qui il tuo commento"></textarea>
+          <textarea class="form-control" id="campotextarea1" rows="3" name="ilcommento" placeholder="Metti qui il tuo commento" required="required"></textarea>
         </fieldset>
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
