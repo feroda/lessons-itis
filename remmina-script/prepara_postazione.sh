425a94b239740271eac4db02d839d07907462fd3
Luca Ferroni <luca@befair.it>
2017-11-14 14:16:27 +0100

    REVIEW -- prepara_postazione.sh: idempotente e fix permessi e nome file

--- remmina-script/prepara_postazione.sh
+++ remmina-script/prepara_postazione.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "DA PROVARE LA RIGA del 'getty' prima di abilitare, ora non ho Ubuntu sottomano"
-exit 10
-
 if [ -z "$1" ]; then
     echo "Usage: $0 <IP VM>";
     exit 100
@@ -13,11 +10,18 @@ IP="$1"
 cp -a systemd-udevd.service.d/ /etc/systemd/system/
 cp 99-usb.rules /etc/udev/rules.d/
 
-mkdir /mnt/usb
+mkdir -p /mnt/usb
 chmod 0666 /mnt/usb
-if [ $(grep chiavetta-usb | wc -l) -eq 0 ]; then
+
+if [ $(grep chiavetta-usb /etc/fstab | wc -l) -eq 0 ]; then
     echo "/dev/chiavetta-usb      /mnt/usb/       auto            rw,relatime,noauto,users   0   0" >> /etc/fstab
 fi
 
-sed -i.bak 's/getty/getty -a utente/g' /etc/systemd/getty@.service
-sed -e "s/10.2.60.100/$IP/g" remmina.conf > /home/utente/.remmina/
+if [ $(grep -- '-a utente' /lib/systemd/system/getty@.service | wc -l) -eq 0 ]; then
+    sed -i.bak 's/getty/getty -a utente/g' /lib/systemd/system/getty@.service
+fi
+
+sed -e "s/10.2.60.100/$IP/g" VM.remmina > /home/utente/.remmina/VM-$IP.remmina
+chown utente:utente /home/utente/.remmina/VM-$IP.remmina
+
+echo "Fatto tutto: ora bisogna mettere 'remmina -c /home/utente/.remmina/VP-$IP.remmina' in autostart del sistema grafico"
